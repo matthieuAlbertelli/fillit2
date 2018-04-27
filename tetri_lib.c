9a3952adae165f4713e94c23d6fdde91514aeb67
Aubane COULOMBIER <acoulomb@student.42.fr>
2018-04-27 19:56:00 +0200

    tetri_lib WIP

---
+++ tetri_lib.c
@@ -0,0 +1,21 @@
+char *ft_tetri_lib()
+{
+	char nb_tetri[];
+	char tetri_i[];
+	char tetri_j[];
+	char tetri_l[];
+	char tetri_o[];
+	char tetri_s[];
+	char tetri_z[];
+	char tetri_t[];
+
+//ft_strjoin(str,
+	nb_tetri = "19\n";
+	tetri_i = "I1 4 1\n#\n#\n#\n#\nI2 1 4\n####\n";
+	tetri_j = "J1 2 3\n###\n..#\nJ2 2 3\n#..\n###\nJ3 3 2\n##\n#.\n#.\nJ4 3 2\n.#\n.#\n##\n";
+	tetri_l = "L1 2 3\n###\n#..\nL2 2 3\n..#\n###\nL3 3 2\n#.\n#.\n##\nL4 3 2\n##\n.#\n.#\n";
+	tetri_o = "O1 2 2\n##\n##\n";
+	tetri_s = "S1 2 3\n.##\n##.\nS2 3 2\n#.\n##\n.#\n";
+	tetri_z = "Z1 2 3\n##.\n.##\nZ2 3 2\n.#\n##\n#.\n";
+	tetri_t = "T1 2 3\n.#.\n###\nT2 2 3\n###\n.#.\nT3 3 2\n#.\n##\n#.\nT4 3 2\n.#\n##\n.#";
+}
