71df5b0c9449690be6febb1ddb7c6ac95550b4e7
Aubane COULOMBIER <acoulomb@student.42.fr>
2018-04-28 17:42:21 +0200

    fonctions commentees WIP3

--- ft_read_tetriminos.c
+++ ft_read_tetriminos.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_read_tetriminos.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malberte <malberte@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acoulomb <acoulomb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/28 14:39:31 by malberte          #+#    #+#             */
-/*   Updated: 2018/04/28 15:31:26 by malberte         ###   ########.fr       */
+/*   Updated: 2018/04/28 17:39:47 by acoulomb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 #include "libft/libft.h"
 #include "tetrimino.h"
 #include "clean.h"
+
+/*
+** This function reads one line in the tetrimino and saves the coordinates
+** of the blocks read. It also checks if the tetrimino declaration is valid
+** (4 blocks,# and .).
+*/
 
 static char		*read_line(t_blocks pos, int *nb_blk, const char *buf, int h)
 {
@@ -42,6 +48,11 @@ static char		*read_line(t_blocks pos, int *nb_blk, const char *buf, int h)
 		ft_exit();
 	return ((char *)(buf + w));
 }
+
+/*
+** This function reads 4lines in the file and returns the tetriminos in a 
+** string.
+*/
 
 static char		*read_4lines(const char *buf, t_blocks pos)
 {
@@ -76,6 +87,12 @@ static char		*check_tetrimino_end(const char *buf)
 	return ((char *)buf);
 }
 
+/*
+** This function reads the next tetriminos, checks if it's the last tetrimino
+** and then launches the pattern recognition. If the tetriminos is valid,
+** a t_tetrimino is allocated whith its information.
+*/
+
 static char		*read_tetrimino(t_tetrimino **new, const char *buf)
 {
 	t_blocks			pos;
@@ -95,6 +112,12 @@ static char		*read_tetrimino(t_tetrimino **new, const char *buf)
 	return ((char*)buf);
 }
 
+/*
+** This function browses the string containing the characters read in the
+** input file and sends to ft_read_tetrimino the begining of each new tetrimino
+** For each tetrimino read, the position is initialised at h -1 and w 0.
+*/
+
 int				ft_read_tetriminos(t_tetrimino **tetri, int *nb_tetri, \
 							const char *buf)
 {
@@ -109,6 +132,7 @@ int				ft_read_tetriminos(t_tetrimino **tetri, int *nb_tetri, \
 		if (*nb_tetri == MAX_TETRIMINOS)
 			ft_exit();
 		pbuf = read_tetrimino(&(tetri[*nb_tetri]), pbuf);
+// pourquoi initialiser ici et pas dans read_tetrimino ?
 		tetri[*nb_tetri]->pos[HEIGHT] = 0;
 		tetri[*nb_tetri]->pos[WIDTH] = -1;
 		++*nb_tetri;
