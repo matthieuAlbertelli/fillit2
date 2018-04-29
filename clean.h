b59239411a67901d0c4eef977d5441dd22d39a23
Aubane COULOMBIER <acoulomb@student.42.fr>
2018-04-29 14:40:36 +0200

    fichiers clean.c et .h a la norme, proto checkes

--- clean.h
+++ clean.h
@@ -6,7 +6,7 @@
 /*   By: acoulomb <acoulomb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/28 12:44:42 by acoulomb          #+#    #+#             */
-/*   Updated: 2018/04/29 13:02:36 by acoulomb         ###   ########.fr       */
+/*   Updated: 2018/04/29 14:40:06 by acoulomb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ extern					t_clean	g_clean;
 ** the program. At any time, if the program needs to exit, the global is freed.
 */
 
-void					ft_clean();
+void					ft_clean(void);
+void					ft_exit(void);
 void					*ft_safe_alloc(size_t size);
-void					ft_exit();
 
 #endif
