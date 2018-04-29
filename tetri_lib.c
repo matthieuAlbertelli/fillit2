a020826b932827bc6da73bc4d9a238297ee6ec5c
Aubane COULOMBIER <acoulomb@student.42.fr>
2018-04-29 14:49:40 +0200

    tetri_lib .c et .h a la norme

--- tetri_lib.c
+++ tetri_lib.c
@@ -6,7 +6,7 @@
 /*   By: acoulomb <acoulomb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 21:07:34 by acoulomb          #+#    #+#             */
-/*   Updated: 2018/04/28 16:19:56 by acoulomb         ###   ########.fr       */
+/*   Updated: 2018/04/29 14:44:39 by acoulomb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "tetri_lib.h"
 
 /*
-** This function creates a string containing information on the 19 tetriminos 
+** This function creates a string containing information on the 19 tetriminos
 ** that exist : their name, height, width and pattern. It is used to generate
 ** the global g_patterns who contains 19 structures t_tetrimino_pattern.
 */
@@ -23,17 +23,17 @@ char	*ft_tetri_lib(void)
 {
 	char *str;
 
-	str = ft_strnew(400);
-	ft_strlcat(str, NB_TETRI_LIB, 400);
-	ft_strlcat(str, TETRI_I, 400);
-	ft_strlcat(str, TETRI_J, 400);
-	ft_strlcat(str, TETRI_J2, 400);
-	ft_strlcat(str, TETRI_L, 400);
-	ft_strlcat(str, TETRI_L2, 400);
-	ft_strlcat(str, TETRI_O, 400);
-	ft_strlcat(str, TETRI_S, 400);
-	ft_strlcat(str, TETRI_Z, 400);
-	ft_strlcat(str, TETRI_T, 400);
-	ft_strlcat(str, TETRI_T2, 400);
+	str = ft_strnew(300);
+	ft_strlcat(str, NB_TETRI_LIB, 300);
+	ft_strlcat(str, TETRI_I, 300);
+	ft_strlcat(str, TETRI_J, 300);
+	ft_strlcat(str, TETRI_J2, 300);
+	ft_strlcat(str, TETRI_L, 300);
+	ft_strlcat(str, TETRI_L2, 300);
+	ft_strlcat(str, TETRI_O, 300);
+	ft_strlcat(str, TETRI_S, 300);
+	ft_strlcat(str, TETRI_Z, 300);
+	ft_strlcat(str, TETRI_T, 300);
+	ft_strlcat(str, TETRI_T2, 300);
 	return (str);
 }
